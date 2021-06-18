8d4167a0b2d284d5e0663791d4b23ede7b1ae821
Rodrigo Santa Eulalia <rsantaeulalia@medallia.com>
2021-06-18 20:32:32 -0300

    added endpoints to short url

--- server.js
+++ server.js
@@ -2,15 +2,10 @@ require('dotenv').config();
 const express = require('express');
 var bodyParser = require('body-parser');
 const cors = require('cors');
-const dns = require('dns');
-const url = require('url'); 
-const LocalStorage = require('node-localstorage'),
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
-var localStorage = new LocalStorage('./scratch'); 
 
 const shortenedUrls = {};
 let numberOfUrls = 0;
@@ -27,7 +22,6 @@ app.get('/', function (req, res) {
 
 app.get('/api/shorturl/:urlId', function (req, res) {
   const urlId = req.params.urlId;
-  shortenedUrls = localStorage.getItem("shortenedUrls");
   if (urlId in shortenedUrls) {
     res.writeHead(301, { Location: shortenedUrls[urlId] });
     res.end();
@@ -39,21 +33,27 @@ app.get('/api/shorturl/:urlId', function (req, res) {
 app.post('/api/shorturl', function (req, res) {
   const originalUrl = req.body.url;
 
-  dns.lookup(url.parse(originalUrl).hostname, err => {
-    if (err && err.code === 'ENOTFOUND') {
-      res.json({ error: 'invalid url' });
-    } else {
-      numberOfUrls = localStorage.getItem("numberOfUrls");
-      numberOfUrls = numberOfUrls++;
-      localStorage.setItem("numberOfUrls", numberOfUrls);
-
-      shortenedUrls = localStorage.getItem("shortenedUrls");
-      shortenedUrls[numberOfUrls] = originalUrl;
-      localStorage.setItem("shortenedUrls", shortenedUrls);
-      res.json({ original_url: originalUrl, short_url: numberOfUrls });
-    }
+  testValidUrl(req.body.url, (err, address) => {
+    if(err) return res.json(err);
+    if (address == null)
+      return res.json({error: 'invalid url'});
+    
+    numberOfUrls = numberOfUrls++;
+    shortenedUrls[numberOfUrls] = originalUrl;
+    res.json({ original_url: originalUrl, short_url: numberOfUrls });
   })
 });
+
+const testValidUrl = (url, done) => {
+  if ( /^https?:\/\/(w{3}.)?[\w-]+.com(\/\w+)*/.test(url) ){
+    dns.lookup(url.replace(/^https?:\/\//, ''), (err, address, family) => {
+      if(err) return done(err);
+    done(null, address);
+    });
+  }
+  else
+    done(null, null);
+}
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
