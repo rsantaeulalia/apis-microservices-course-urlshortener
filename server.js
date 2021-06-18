92d51c65098c115a5a597f6cac10ebd1641e8390
Rodrigo Santa Eulalia <rsantaeulalia@medallia.com>
2021-06-18 15:39:35 -0300

    added endpoints to short url

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 var bodyParser = require('body-parser');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
