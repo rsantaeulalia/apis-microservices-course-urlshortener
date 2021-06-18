cd59ce78720de3d17008dd56cbc0d11d9c1f181c
Rodrigo Santa Eulalia <rsantaeulalia@medallia.com>
2021-06-18 15:29:26 -0300

    added endpoints to short url

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+var bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const shortenedUrls = {};
+
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,9 +19,26 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/:urlId', function(req, res) {
+  const urlId = req.params.urlId;
+  if(urlId in shortenedUrls){
+    window.location.href = shortenedUrls[urlId];
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.original_url;
+  const shortUrl = req.body.short_url;
+
+  dns.lookup(originalUrl, (err, addr) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      shortenedUrls[short_url] = originalUrl;
+      res.json({ original_url: originalUrl, short_url: shortUrl });
+    }})
 });
 
 app.listen(port, function() {
