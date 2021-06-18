8db9ae07c5e2356821f06185548664ee50903895
Rodrigo Santa Eulalia <rsantaeulalia@medallia.com>
2021-06-18 20:25:06 -0300

    added endpoints to short url

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ var bodyParser = require('body-parser');
 const cors = require('cors');
 const dns = require('dns');
 const url = require('url'); 
+const LocalStorage = require('node-localstorage'),
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+var localStorage = new LocalStorage('./scratch'); 
 
 const shortenedUrls = {};
 let numberOfUrls = 0;
@@ -24,6 +27,7 @@ app.get('/', function (req, res) {
 
 app.get('/api/shorturl/:urlId', function (req, res) {
   const urlId = req.params.urlId;
+  shortenedUrls = localStorage.getItem("shortenedUrls");
   if (urlId in shortenedUrls) {
     res.writeHead(301, { Location: shortenedUrls[urlId] });
     res.end();
@@ -39,8 +43,13 @@ app.post('/api/shorturl', function (req, res) {
     if (err && err.code === 'ENOTFOUND') {
       res.json({ error: 'invalid url' });
     } else {
+      numberOfUrls = localStorage.getItem("numberOfUrls");
       numberOfUrls = numberOfUrls++;
+      localStorage.setItem("numberOfUrls", numberOfUrls);
+
+      shortenedUrls = localStorage.getItem("shortenedUrls");
       shortenedUrls[numberOfUrls] = originalUrl;
+      localStorage.setItem("shortenedUrls", shortenedUrls);
       res.json({ original_url: originalUrl, short_url: numberOfUrls });
     }
   })
